55706a7a55888d6da0451a9a2b86dbe60be8280a
Lucas Carvalho <lucasddmc@outlook.com>
2024-05-07 16:10:46 -0300

    Adiciona arquivos do projeto

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Lucas Dantas de Moura Carvalho on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

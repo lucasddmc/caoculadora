a76f61826201348fd9f2ae353fcd24f7e09ac8a8
Lucas Carvalho <lucasddmc@outlook.com>
2024-05-07 17:39:15 -0300

    Implementa TextFields

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var anos: Int? = nil
+    @State var meses: Int? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "pawprint")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Qual a idade do seu cão?")
+            Text("Anos")
+            TextField(
+                "Digite a idade do seu cão em anos caninos",
+                value: $anos,
+                format: .number
+            )
+            
+            Text("Meses")
+            TextField(
+                "E quantos meses além disso ele tem",
+                value: $meses,
+                format: .number
+            )
         }
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .bold()
+        .fontDesign(.rounded)
         .padding()
     }
 }

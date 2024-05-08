258401412612cea8828aa21675c550c38358107f
Lucas Carvalho <lucasddmc@outlook.com>
2024-05-08 18:01:58 -0300

    Add new button, and clarinha's image

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var anos: Int? = nil
     @State var meses: Int? = nil
+    @State var result: Int?
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -20,13 +21,35 @@ struct ContentView: View {
                 value: $anos,
                 format: .number
             )
-            
             Text("Meses")
             TextField(
                 "E quantos meses além disso ele tem",
                 value: $meses,
                 format: .number
             )
+            Text("Porte")
+            // aqui vai o segmented control
+            if let result = result {
+                Text("Seu cachorro tem, em idade humana: ")
+                Text("\(result) anos")
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+                    .shadow(radius: 5, x: 5, y: 5)
+            }
+            Button(action: {
+                result = 23
+            }, label: {
+                Text("Cãocular")
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.purple)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+            })
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)

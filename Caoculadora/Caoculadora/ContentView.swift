79ff1915caf696f288bd242d10199fba64574e65
Lucas Carvalho <lucasddmc@outlook.com>
2024-05-09 17:50:41 -0300

    Implementa segmented control, espaçamento e calculo da idade

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -7,28 +7,47 @@
 
 import SwiftUI
 
+enum Size: String, CaseIterable {
+    case small = "Pequeno"
+    case medium = "Médio"
+    case large = "Grande"
+}
+
 struct ContentView: View {
-    @State var anos: Int? = nil
-    @State var meses: Int? = nil
+    @State var years: Int? = nil
+    @State var months: Int? = nil
     @State var result: Int?
+    @State var size: Size?
+    
     
     var body: some View {
         VStack(alignment: .leading) {
+            Spacer()
             Text("Qual a idade do seu cão?")
             Text("Anos")
             TextField(
                 "Digite a idade do seu cão em anos caninos",
-                value: $anos,
+                value: $years,
                 format: .number
             )
             Text("Meses")
             TextField(
                 "E quantos meses além disso ele tem",
-                value: $meses,
+                value: $months,
                 format: .number
             )
             Text("Porte")
-            // aqui vai o segmented control
+            
+            Picker(selection: $size, label: Text("Porte")) {
+                ForEach(Size.allCases, id: \.self) { value in
+                    Text(value.rawValue)
+                        .tag(value as Size?)
+                }
+            }
+            .pickerStyle(.segmented)
+            Divider()
+            Spacer()
+            
             if let result = result {
                 Text("Seu cachorro tem, em idade humana: ")
                 Text("\(result) anos")
@@ -40,13 +59,12 @@ struct ContentView: View {
                     .frame(maxWidth: .infinity)
                     .shadow(radius: 5, x: 5, y: 5)
             }
-            Button(action: {
-                result = 23
-            }, label: {
+            Spacer()
+            Button(action: processYears, label: {
                 Text("Cãocular")
                     .frame(height: 50)
                     .frame(maxWidth: .infinity)
-                    .background(Color.purple)
+                    .background(Color.teal)
                     .foregroundStyle(.white)
                     .clipShape(.rect(cornerRadius: 10))
             })
@@ -56,6 +74,43 @@ struct ContentView: View {
         .bold()
         .fontDesign(.rounded)
         .padding()
+    }
+    
+    
+    func processYears() {
+//        if let years {
+//            result = years * 7
+//        } else {
+//            print("preencha o campo de entrada")
+//        }
+        
+        guard let years else {
+            print("preencha o campo de anos")
+            return
+        }
+        
+        guard let months else {
+            print("preencha o campo de meses")
+            return
+        }
+        
+        guard years > 0 || months > 0 else {
+            print("algum campo tem que ter valor maior que 0")
+            return
+        }
+        
+        result = (years + (months / 12)) * 7
+        
+        guard var result else {
+            print("Ocorreu um erro")
+            return
+        }
+        
+        if size == .large {
+            self.result = Int(Double(result) * 0.8)
+        } else if size == .medium {
+            self.result = Int(Double(result) * 0.9)
+        }
     }
 }
 
